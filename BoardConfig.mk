4c7e1d504c0941fd48d7fe72eb78ea92f4547db3
Aragaki <4ragaki@gmail.com>
2023-11-05 17:49:51 +0800

    add double tap wake

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -176,6 +176,7 @@ TARGET_BOARD_PLATFORM := kona
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
