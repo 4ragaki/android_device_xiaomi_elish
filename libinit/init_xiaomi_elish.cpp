4f9bdde2c76716f1ddcea5f78593a954fc2e6a0d
Aragaki <4ragaki@gmail.com>
2023-11-05 16:15:04 +0800

    libinit: make recovery debuggable

--- libinit/init_xiaomi_elish.cpp
+++ libinit/init_xiaomi_elish.cpp
@@ -1,30 +1,28 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
+ * Copyright (C) 2023 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <libinit_dalvik_heap.h>
-#include <libinit_variant.h>
-
 #include "vendor_init.h"
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
-static const variant_info_t elish_info = {
-    .hwc_value = "",
-    .sku_value = "",
-
-    .brand = "Xiaomi",
-    .device = "elish",
-    .marketname = "Xiaomi pad 5Pro",
-    .model = "M2105K81AC",
-    .build_fingerprint = "Xiaomi/elish/elish:13/TKQ1.221013.002/V14.0.4.0.TKYCNXM:user/release-keys"
-};
-
-static const std::vector<variant_info_t> variants = {
-    elish_info
-};
+void property_override(std::string prop, std::string value) {
+    auto pi = (prop_info *) __system_property_find(prop.c_str());
+    if (pi != nullptr) {
+        __system_property_update(pi, value.c_str(), value.length());
+    } else {
+        __system_property_add(prop.c_str(), prop.length(), value.c_str(), value.length());
+    }
+}
 
 void vendor_load_properties() {
-    search_variant(variants);
     set_dalvik_heap();
+
+#ifdef __ANDROID_RECOVERY__
+    property_override("ro.debuggable", "1");
+    property_override("ro.adb.secure.recovery", "0");
+#endif
 }
