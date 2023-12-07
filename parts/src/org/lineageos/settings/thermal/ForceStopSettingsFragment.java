5b215d58e194545cc1a5061f64e446126750e8a6
Aragaki <4ragaki@gmail.com>
2023-12-07 14:42:29 +0800

    parts: add freeze extra components to force-stop

--- parts/src/org/lineageos/settings/thermal/ForceStopSettingsFragment.java
+++ parts/src/org/lineageos/settings/thermal/ForceStopSettingsFragment.java
@@ -16,18 +16,16 @@
 package org.lineageos.settings.thermal;
 
 import android.annotation.Nullable;
-import android.content.Context;
-import android.content.Intent;
+import android.content.ComponentName;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -36,25 +34,40 @@ import androidx.annotation.NonNull;
 import androidx.preference.PreferenceFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.ConcatAdapter;
 
 import com.android.settingslib.applications.ApplicationsState;
 
 import org.lineageos.settings.R;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ForceStopSettingsFragment extends PreferenceFragment
         implements ApplicationsState.Callbacks {
 
-    private UserPackagesAdapter mAllPackagesAdapter;
+    private ConcatAdapter mRVAdapter;
+    private UserPackagesAdapter mUserPackagesAdapter;
+    private ExtraComponentsAdapter mExtraAdapter;
+    private PackageManager mPm;
     private ApplicationsState mApplicationsState;
     private ApplicationsState.Session mSession;
-    private ActivityFilter mActivityFilter;
-    private Map<String, ApplicationsState.AppEntry> mEntryMap =
-            new HashMap<String, ApplicationsState.AppEntry>();
+    private AppFilter mAppFilter;
+    private List<String> mUserApps = new ArrayList<>();
+
+    private ComponentName mExtraComponents[] = new ComponentName[] {
+        new ComponentName(
+            "com.google.android.gms",
+            "com.google.android.gms.chimera.GmsIntentOperationService"
+        )
+    };
 
     private RecyclerView mAppsRecyclerView;
 
@@ -71,9 +84,13 @@ public class ForceStopSettingsFragment extends PreferenceFragment
         mApplicationsState = ApplicationsState.getInstance(getActivity().getApplication());
         mSession = mApplicationsState.newSession(this);
         mSession.onResume();
-        mActivityFilter = new ActivityFilter(getActivity().getPackageManager());
 
-        mAllPackagesAdapter = new UserPackagesAdapter(getActivity());
+        mPm = getActivity().getPackageManager();
+        mAppFilter = new AppFilter(mPm);
+
+        mUserPackagesAdapter = new UserPackagesAdapter();
+        mExtraAdapter = new ExtraComponentsAdapter();
+        mRVAdapter = new ConcatAdapter(mUserPackagesAdapter, mExtraAdapter);
 
         mThermalUtils = new ThermalUtils(getActivity());
         mThermalUtils.initialStopSet(getActivity());
@@ -91,7 +108,7 @@ public class ForceStopSettingsFragment extends PreferenceFragment
 
         mAppsRecyclerView = view.findViewById(R.id.thermal_rv_view);
         mAppsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mAppsRecyclerView.setAdapter(mAllPackagesAdapter);
+        mAppsRecyclerView.setAdapter(mRVAdapter);
     }
 
 
@@ -112,7 +129,7 @@ public class ForceStopSettingsFragment extends PreferenceFragment
 
     @Override
     public void onPackageListChanged() {
-        mActivityFilter.updateLauncherInfoList();
+        mAppFilter.updateLauncherInfoList();
         rebuild();
     }
 
@@ -120,7 +137,7 @@ public class ForceStopSettingsFragment extends PreferenceFragment
     public void onRebuildComplete(ArrayList<ApplicationsState.AppEntry> entries) {
         if (entries != null) {
             handleAppEntries(entries);
-            mAllPackagesAdapter.notifyDataSetChanged();
+            mUserPackagesAdapter.notifyDataSetChanged();
         }
     }
 
@@ -152,13 +169,12 @@ public class ForceStopSettingsFragment extends PreferenceFragment
     private void handleAppEntries(List<ApplicationsState.AppEntry> entries) {
         final ArrayList<String> sections = new ArrayList<String>();
         final ArrayList<Integer> positions = new ArrayList<Integer>();
-        final PackageManager pm = getActivity().getPackageManager();
         String lastSectionIndex = null;
         int offset = 0;
 
         for (int i = 0; i < entries.size(); i++) {
             final ApplicationInfo info = entries.get(i).info;
-            final String label = (String) info.loadLabel(pm);
+            final String label = (String) info.loadLabel(mPm);
             final String sectionIndex;
 
             if (!info.enabled) {
@@ -179,25 +195,42 @@ public class ForceStopSettingsFragment extends PreferenceFragment
             offset++;
         }
 
-        mAllPackagesAdapter.setEntries(entries, sections, positions);
-        mEntryMap.clear();
-        for (ApplicationsState.AppEntry e : entries) {
-            mEntryMap.put(e.info.packageName, e);
-        }
+        List<ApplicationsState.AppEntry> userEntries =
+                entries.stream().filter(appEntry -> mUserApps.stream()
+                        .anyMatch(p -> p.equals(appEntry.info.packageName)))
+                        .collect(Collectors.toList());
+        mUserPackagesAdapter.setEntries(userEntries);
+
+        List<ApplicationsState.AppEntry> extraEntries =
+                entries.stream().filter(appEntry -> Arrays.stream(mExtraComponents)
+                        .anyMatch(p -> p.getPackageName().equals(appEntry.info.packageName)))
+                        .collect(Collectors.toList());
+        mExtraAdapter.setEntries(extraEntries);
     }
 
     private void rebuild() {
-        mSession.rebuild(mActivityFilter, ApplicationsState.ALPHA_COMPARATOR);
+        mSession.rebuild(mAppFilter, ApplicationsState.ALPHA_COMPARATOR);
     }
 
-    private class ViewHolder extends RecyclerView.ViewHolder {
+    private class CategoryViewHolder extends RecyclerView.ViewHolder {
+        private TextView title;
+
+        private CategoryViewHolder(View view) {
+            super(view);
+            this.title = view.findViewById(R.id.category_name);
+
+            view.setTag(this);
+        }
+    }
+
+    private class AppViewHolder extends RecyclerView.ViewHolder {
         private TextView title;
         private TextView status;
         private ImageView icon;
         private View rootView;
         private Switch preference;
 
-        private ViewHolder(View view) {
+        private AppViewHolder(View view) {
             super(view);
             this.title = view.findViewById(R.id.app_name);
             this.status = view.findViewById(R.id.app_status);
@@ -209,63 +242,63 @@ public class ForceStopSettingsFragment extends PreferenceFragment
         }
     }
 
-    private class UserPackagesAdapter extends RecyclerView.Adapter<ViewHolder>
+    private class UserPackagesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
             implements View.OnClickListener {
 
         private List<ApplicationsState.AppEntry> mEntries = new ArrayList<>();
-        private String[] mSections;
-        private int[] mPositions;
-
-        public UserPackagesAdapter(Context context) {
-            mActivityFilter = new ActivityFilter(context.getPackageManager());
-        }
 
         @Override
         public int getItemCount() {
-            return mEntries.size();
+            return mEntries.size() + 1;
         }
 
         @Override
-        public long getItemId(int position) {
-            return mEntries.get(position).id;
+        public int getItemViewType(int position) {
+            return position;
         }
 
         @NonNull
         @Override
-        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            ViewHolder holder = new ViewHolder(LayoutInflater.from(parent.getContext())
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            if (viewType == 0) {
+                CategoryViewHolder holder = new CategoryViewHolder(LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.forcestop_category_item, parent, false));
+                return holder;
+            }
+            AppViewHolder holder = new AppViewHolder(LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.forcestop_list_item, parent, false));
             holder.preference.setOnClickListener(this);
             return holder;
         }
 
         @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            ApplicationsState.AppEntry entry = mEntries.get(position);
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+            if (holder instanceof CategoryViewHolder) {
+                ((CategoryViewHolder) holder).title.setText(R.string.forcestop_category_stop);
+                return;
+            }
+
+            ApplicationsState.AppEntry entry = mEntries.get(position - 1);
 
             if (entry == null) {
                 return;
             }
 
-            holder.title.setText(entry.label);
-            holder.rootView.setOnClickListener(v -> holder.preference.performClick());
+            AppViewHolder appViewHolder = (AppViewHolder) holder;
+            appViewHolder.title.setText(entry.label);
+            appViewHolder.rootView.setOnClickListener(v -> appViewHolder.preference.performClick());
             mApplicationsState.ensureIcon(entry);
-            holder.icon.setImageDrawable(entry.icon);
+            appViewHolder.icon.setImageDrawable(entry.icon);
 
             boolean packageState = mThermalUtils.getForceStopStateForPackage(entry.info.packageName);
-            holder.status.setText(packageState ? R.string.forcestop_enabled : R.string.forcestop_disabled);
-            holder.preference.setTag(entry);
-            holder.preference.setChecked(packageState);
+            appViewHolder.status.setText(packageState ? R.string.forcestop_enabled : R.string.forcestop_disabled);
+            appViewHolder.preference.setTag(entry);
+            appViewHolder.preference.setChecked(packageState);
         }
 
-        private void setEntries(List<ApplicationsState.AppEntry> entries,
-                                List<String> sections, List<Integer> positions) {
+        private void setEntries(List<ApplicationsState.AppEntry> entries) {
             mEntries = entries;
-            mSections = sections.toArray(new String[sections.size()]);
-            mPositions = new int[positions.size()];
-            for (int i = 0; i < positions.size(); i++) {
-                mPositions[i] = positions.get(i);
-            }
             notifyDataSetChanged();
         }
 
@@ -277,34 +310,114 @@ public class ForceStopSettingsFragment extends PreferenceFragment
         }
     }
 
-    private class ActivityFilter implements ApplicationsState.AppFilter {
+    private class ExtraComponentsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
+            implements View.OnClickListener {
+
+        private List<ApplicationsState.AppEntry> mEntries = new ArrayList<>();
+
+        @Override
+        public int getItemCount() {
+            return mExtraComponents.length + 1;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return position;
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            if (viewType == 0) {
+                CategoryViewHolder holder = new CategoryViewHolder(LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.forcestop_category_item, parent, false));
+                return holder;
+            }
+            AppViewHolder holder = new AppViewHolder(LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.forcestop_list_item, parent, false));
+            holder.preference.setOnClickListener(this);
+            return holder;
+        }
+
+
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            if (holder instanceof  CategoryViewHolder) {
+                ((CategoryViewHolder) holder).title.setText(R.string.forcestop_category_freeze);
+                return;
+            }
+
+            ComponentName comp = mExtraComponents[position - 1];
+
+            if (comp == null) {
+                return;
+            }
+
+            AppViewHolder appViewHolder = (AppViewHolder) holder;
+            appViewHolder.title.setText(comp.flattenToShortString());
+            appViewHolder.rootView.setOnClickListener(v -> appViewHolder.preference.performClick());
+
+            Optional<ApplicationsState.AppEntry> entry = mEntries.stream()
+                    .filter(p -> p.info.packageName.equals(comp.getPackageName()))
+                    .findAny();
+            if (entry.isPresent()) {
+                ApplicationsState.AppEntry appEntry = entry.get();
+                mApplicationsState.ensureIcon(appEntry);
+                appViewHolder.icon.setImageDrawable(appEntry.icon);
+            }
+
+            boolean disabled = mPm.getComponentEnabledSetting(comp) == PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+            appViewHolder.status.setText(disabled ? R.string.forcestop_frozen : R.string.forcestop_freeze_disabled);
+            appViewHolder.preference.setTag(comp);
+            appViewHolder.preference.setChecked(disabled);
+        }
+
+        @Override
+        public void onClick(View pref) {
+            final ComponentName component = (ComponentName) pref.getTag();
+            mPm.setComponentEnabledSetting(
+                component, ((Switch)pref).isChecked() ? PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+                : PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.SYNCHRONOUS
+            );
+            notifyDataSetChanged();
+        }
+
+        private void setEntries(List<ApplicationsState.AppEntry> entries) {
+            mEntries = entries;
+            notifyDataSetChanged();
+        }
+    }
+
+
+    private class AppFilter implements ApplicationsState.AppFilter {
 
         private final PackageManager mPackageManager;
-        private final List<String> mLauncherResolveInfoList = new ArrayList<String>();
+        private final List<String> mPackageRequired = new ArrayList<String>();
 
-        private ActivityFilter(PackageManager packageManager) {
+        private AppFilter(PackageManager packageManager) {
             this.mPackageManager = packageManager;
 
             updateLauncherInfoList();
         }
 
         public void updateLauncherInfoList() {
-            Intent i = new Intent(Intent.ACTION_MAIN);
-            i.addCategory(Intent.CATEGORY_LAUNCHER);
-            List<ResolveInfo> resolveInfoList = mPackageManager.queryIntentActivities(i, 0);
+            List<PackageInfo> packages = mPackageManager.getInstalledPackages(0);
 
-            synchronized (mLauncherResolveInfoList) {
-                mLauncherResolveInfoList.clear();
-                for (ResolveInfo ri : resolveInfoList) {
-                    try {
-                        PackageInfo info = mPackageManager.getPackageInfo(ri.activityInfo.packageName, PackageManager.PackageInfoFlags.of(0));
+            synchronized (mPackageRequired) {
+                mPackageRequired.clear();
+                mUserApps.clear();
 
-                        if ((info.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0)
-                            mLauncherResolveInfoList.add(ri.activityInfo.packageName);
-                    } catch (PackageManager.NameNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                }
+                packages.forEach( p -> {
+                    boolean c = Arrays.stream(mExtraComponents).anyMatch(cn ->
+                            cn.getPackageName().equals(p.packageName)
+                    );
+
+                    if ((p.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0){
+                        mPackageRequired.add(p.packageName);
+                        mUserApps.add(p.packageName);
+                    } else if (c) mPackageRequired.add(p.packageName);
+                });
             }
         }
 
@@ -314,10 +427,10 @@ public class ForceStopSettingsFragment extends PreferenceFragment
 
         @Override
         public boolean filterApp(ApplicationsState.AppEntry entry) {
-            boolean show = !mAllPackagesAdapter.mEntries.contains(entry.info.packageName);
+            boolean show = !mUserPackagesAdapter.mEntries.contains(entry.info.packageName);
             if (show) {
-                synchronized (mLauncherResolveInfoList) {
-                    show = mLauncherResolveInfoList.contains(entry.info.packageName);
+                synchronized (mPackageRequired) {
+                    show = mPackageRequired.contains(entry.info.packageName);
                 }
             }
             return show;
