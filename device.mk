2b2631de61c1df399459fca433848d6b7bdf5fc8
Aragaki <4ragaki@gmail.com>
2024-03-23 16:14:28 +0800

    elish: drop audio changes
    Revert "parts: introduce dolby atmos"
    This reverts commit 8213309b035bbf570bde04715e4470bda9f95e49.
    
    Revert "sm8250-common: Import Dolby audio blobs"
    This reverts commit 78b3ad12fcc5d1dbe2f2f92441413ec865f12ad1.
    
    Revert "sm8250-common: Sepolicy: Import Latest Dolby@2.0 Sepolicy"
    This reverts commit 9635a9f0b32d9df77716d9af261d1919badba661.
    
    Revert "elish: update audio configs"
    This reverts commit dd01c3ab19bff2bd0c7b10ecf25541addaacd2ea.

--- device.mk
+++ device.mk
@@ -296,12 +296,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
-    libavservices_minijail_vendor \
-    libcodec2_soft_common.vendor \
-    libsfplugin_ccodec_utils.vendor
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
+    libavservices_minijail_vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
